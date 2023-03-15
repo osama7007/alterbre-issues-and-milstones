3f76fd400d8d6eb623ddc232fb0258f39c9747e3
enas-sello <enas.awad169@gmail.com>
2023-03-15 16:25:38 +0200

    style fix

--- src/components/molecules/DropFile.tsx
+++ src/components/molecules/DropFile.tsx
@@ -210,77 +210,83 @@ export const DropFile = ({ name }: DropFileProps_TP) => {
                   className="text-red-500"
                 />
               </div>
-              {/* Delete  images*/}
-              <div className="flex items-center justify-center gap-2">
-                {!!images.length && (
-                  <>
-                    <div className="flex flex-col  gap-2 justify-center">
-                      <span className="text-[8px] text-gray-700">
-                        عرض الصور
-                      </span>
-                      <div className="bg-lightGray rounded-md p-1 relative ">
-                        <div
-                          onClick={() => setLightboxOpen(true)}
-                          className="cursor-pointer flex items-center justify-center p-2 "
-                        >
-                          <span className=" absolute -top-2 right-0 bg-mainGreen px-2 py-1 text-[7px] rounded-full text-white">
-                            {images.length}
-                          </span>
-                          <MdPhotoSizeSelectActual className="w-5 h-5 fill-gray-700" />
+              <div className="flex flex-col gap-1">
+                {/* Delete  images*/}
+                <div className="flex items-center justify-center gap-2">
+                  {!!images.length && (
+                    <>
+                      <div className="flex flex-col  gap-1 justify-center">
+                        <span className="text-[8px] text-gray-700">
+                          عرض الصور
+                        </span>
+                        <div className="bg-lightGray rounded-md p-1 relative ">
+                          <div
+                            onClick={() => setLightboxOpen(true)}
+                            className="cursor-pointer flex items-center justify-center p-2 "
+                          >
+                            <span className=" absolute -top-1 -right-3 bg-mainGreen px-2 py-1 text-[7px] rounded-full text-white">
+                              {images.length}
+                            </span>
+                            <MdPhotoSizeSelectActual className="w-5 h-5 fill-gray-700" />
+                          </div>
                         </div>
                       </div>
-                    </div>
-                    {/* change  to atoms */}{" "}
-                    <div className="flex flex-col  gap-2 justify-center">
-                      <span className="text-[8px] text-gray-700">حذف الكل</span>
-                      <div className="bg-lightGray rounded-md p-3 ">
-                        <AiFillDelete
-                          className="w-5 h-5 fill-mainRed"
-                          onClick={deleteAllImagesHandler}
-                        />
-                        {/* <Button action={deleteAllImagesHandler} variant="danger">
+                      {/* change  to atoms */}{" "}
+                      <div className="flex flex-col  gap-1 justify-center">
+                        <span className="text-[8px] text-gray-700">
+                          حذف الكل
+                        </span>
+                        <div className="bg-lightGray rounded-md p-3 ">
+                          <AiFillDelete
+                            className="w-5 h-5 fill-mainRed"
+                            onClick={deleteAllImagesHandler}
+                          />
+                          {/* <Button action={deleteAllImagesHandler} variant="danger">
                       <AiFillDelete className="w-5 h-5" />
                     </Button> */}{" "}
-                      </div>{" "}
-                    </div>
-                  </>
-                )}
-              </div>
-              {/* Delete pdfs*/}
-              <div className="flex items-center justify-center gap-2">
-                {!!pdfs.length && (
-                  <>
-                    <div className="flex flex-col  gap-2 justify-center">
-                      <span className="text-[8px] text-gray-700">
-                        عرض الصور
-                      </span>
-                      <div className="bg-lightGray rounded-md p-1 relative">
-                        <div
-                          onClick={() => setManyPdfsOpen(true)}
-                          className="cursor-pointer flex items-center justify-center p-2 "
-                        >
-                          <span className=" absolute -top-2 right-0 bg-mainGreen px-2 py-1 text-[7px] rounded-full text-white">
-                            {pdfs.length}
-                          </span>
-                          <BsFiletypePdf className="w-5 h-5 fill-gray-700" />
+                        </div>{" "}
+                      </div>
+                    </>
+                  )}
+                </div>
+                {/* Delete pdfs*/}
+                <div className="flex items-center justify-center gap-2">
+                  {!!pdfs.length && (
+                    <>
+                      <div className="flex flex-col  gap-1 justify-center">
+                        <span className="text-[8px] text-gray-700">
+                          عرض الصور
+                        </span>
+                        <div className="bg-lightGray rounded-md p-1 relative">
+                          <div
+                            onClick={() => setManyPdfsOpen(true)}
+                            className="cursor-pointer flex items-center justify-center p-2 "
+                          >
+                            <span className=" absolute -top-1 -right-3 bg-mainGreen px-2 py-1 text-[7px] rounded-full text-white">
+                              {pdfs.length}
+                            </span>
+                            <BsFiletypePdf className="w-5 h-5 fill-gray-700" />
+                          </div>
                         </div>
                       </div>
-                    </div>
-                    {/* change  to atoms */}{" "}
-                    <div className="flex flex-col  gap-2 justify-center">
-                      <span className="text-[8px] text-gray-700">حذف الكل</span>
-                      <div className="bg-lightGray rounded-md p-3 ">
-                        <AiFillDelete
-                          className="w-5 h-5 fill-mainRed"
-                          onClick={deleteAllPdfsHandler}
-                        />
-                        {/* <Button action={deleteAllPdfsHandler} variant="danger">
+                      {/* change  to atoms */}{" "}
+                      <div className="flex flex-col  gap-1 justify-center">
+                        <span className="text-[8px] text-gray-700">
+                          حذف الكل
+                        </span>
+                        <div className="bg-lightGray rounded-md p-3 ">
+                          <AiFillDelete
+                            className="w-5 h-5 fill-mainRed"
+                            onClick={deleteAllPdfsHandler}
+                          />
+                          {/* <Button action={deleteAllPdfsHandler} variant="danger">
                       Delete all pdfs
                     </Button> */}
+                        </div>
                       </div>
-                    </div>
-                  </>
-                )}
+                    </>
+                  )}
+                </div>
               </div>
               {/* FOR preview THUMBNAILS */}
               {/* {!!thumbs.length && (
