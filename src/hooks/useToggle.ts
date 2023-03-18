40b8d4b346db931914b8ae9f3f31ca8a6a4213c6
Khalid Elborai <elboraikhalid@gmail.com>
2023-03-18 17:45:22 +0200

    fix useToggle type

--- src/hooks/useToggle.ts
+++ src/hooks/useToggle.ts
@@ -4,5 +4,5 @@ export default function useToggle(initialValue = false) {
   const toggle = useCallback(() => {
     setValue((v) => !v)
   }, [])
-  return [value, toggle]
+  return [value, toggle] as const
 }
