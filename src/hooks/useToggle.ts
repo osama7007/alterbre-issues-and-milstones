3bb24212dd744b99d9d56d7b4ac849ffb5303604
Khalid Elborai <elboraikhalid@gmail.com>
2023-03-18 17:39:01 +0200

    fix useToggle

--- src/hooks/useToggle.ts
+++ src/hooks/useToggle.ts
@@ -1,8 +1,8 @@
-// useToggle hook
-import { useState } from "react"
-
-export const useToggle = () => {
-  const [state, setState] = useState(false)
-  const toggle = () => setState(!state)
-  return [state, toggle]
+import { useCallback, useState } from "react"
+export default function useToggle(initialValue = false) {
+  const [value, setValue] = useState(initialValue)
+  const toggle = useCallback(() => {
+    setValue((v) => !v)
+  }, [])
+  return [value, toggle]
 }
